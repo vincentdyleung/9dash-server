88dd25823a626f93c12248551b7a1ce5f3b2ed4d
Sam Siu <tmc03186@gmail.com>
2014-04-26 23:42:02 +0800

    Added SeedDB, search

---
+++ seed.js
@@ -0,0 +1,60 @@
+function seedRes(name,col,num)
+{
+  var target = db.getSiblingDB(name).getCollection(col);
+  target.drop();
+
+  var first = new Array("Good ", "Happy ", "Yummy ");
+  var food = new Array("Fast food ", "Steak ", "Hotpot ");
+  var name = new Array("House", "Restaurant", "Cafe");
+
+  for(var i = 0; i < num; i++) 
+  {
+    var a = first[Math.floor(Math.random() * first.length)];
+    var b = food[Math.floor(Math.random() * food.length)];
+    var c = name[Math.floor(Math.random() * name.length)];
+    var reportNum = 20+Math.floor(Math.random() * 80);
+    var report = new Array();
+    for(var j = 0; j < reportNum; j++)
+    {
+      var userID = 1+Math.floor(Math.random()*100);
+      var reportTime = new Date();
+      var randTime = Math.floor(Math.random()*30);
+      report[j] = 
+        {
+          waiting_time : 1+Math.floor(Math.random()*20),
+          waiting_position : 1+Math.floor(Math.random()*10),
+          waiting_people: 1+Math.floor(Math.random()*5),
+          user: 1+Math.floor(Math.random()*100),
+          submit_time: new Date(reportTime.getTime() + randTime * 60000),
+          user: userID,
+        };
+    }
+
+    target.insert(
+      {
+        name: a+b+c,
+        reports: report,
+      });
+
+  }
+}
+
+function seedUser(name,col,num)
+{
+  var target = db.getSiblingDB(name).getCollection(col);
+  target.drop();
+
+  for(var i = 0; i< num; i++)
+  {
+    target.insert(
+      {
+        name: "user_"+i,
+        fbid: Math.floor(Math.random()*100000000000),
+        point:100+Math.floor(Math.random()*100)*100
+      });
+  }
+}
+
+seedRes("9dash", "restaurants", 100);
+seedUser("9dash", "users", 100);
+
